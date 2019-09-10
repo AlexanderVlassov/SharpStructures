76aa7a9c4ff053ccd76091a0595d1ad6107424d6
vlassov <vlassov@skbkontur.ru>
2019-09-10 15:33:07 +0500

    api: EnumerableExtensions

---
+++ Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Common.Extensions
+{
+    [UsedImplicitly]
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+            {
+                action(item);
+                yield return item;
+            }
+        }
+    }
+}
