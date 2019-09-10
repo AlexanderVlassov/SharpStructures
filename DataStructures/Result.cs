6c3a3af7f025da178031d7698da9e7c83e3eb4cd
vlassov <vlassov@skbkontur.ru>
2019-09-10 20:13:04 +0500

    api: Result<T>

---
+++ DataStructures/Result.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace DataStructures
+{
+    public class Result<T>
+    {
+        public static Result<T> Success(T obj)
+        {
+            return new Result<T>(obj, true, null);
+        }
+
+        public static Result<T> Failure(Exception exception)
+        {
+            return new Result<T>(default(T), false, exception);
+        }
+
+        private Result(T value, bool isSuccess, Exception exception)
+        {
+            IsSuccess = isSuccess;
+            Exception = exception;
+            Value = value;
+        }
+
+        public T Value { get; }
+        public bool IsSuccess { get; }
+        public Exception Exception { get; }
+    }
+}
